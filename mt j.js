66cadc09c0a323dabab69c6462c24dc4a314aa66
loki5129 <123837505+loki5129@users.noreply.github.com>
2024-02-07 15:24:47 +0000

    Add files via upload

---
+++ mt j.js
@@ -0,0 +1,9 @@
+function para() {
+  document.getElementById("para").innerHTML = "paragath";
+}
+function image() {
+  document.getElementById("image").style.display = "none";
+}
+function image0() {
+  document.getElementsByClassName("img").style.switch.display = "true";
+}
